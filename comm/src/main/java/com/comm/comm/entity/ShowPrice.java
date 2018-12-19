8d087e5adef9a8d72561900df0b61fc6e7a781a1
a519894574 <519894574@qq.com>
2018-12-19 09:15:17 +0800

    取消JPA

--- comm/src/main/java/com/comm/comm/entity/ShowPrice.java
+++ comm/src/main/java/com/comm/comm/entity/ShowPrice.java
@@ -8,11 +8,35 @@ public class ShowPrice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//自动增长
     @Column(name = "showP_id", columnDefinition = "integer(11) COMMENT '演出价位ID'")
-    private int id;
+    private int showP_id;
 
     @Column(name = "show_id", columnDefinition = "integer(11) COMMENT '演出ID'")
     private int show_id;
 
     @Column(name = "showP_price", columnDefinition = "double COMMENT '演出价格'")
-    private double showPrice;
+    private double showP_price;
+
+    public int getShowP_id() {
+        return showP_id;
+    }
+
+    public void setShowP_id(int showP_id) {
+        this.showP_id = showP_id;
+    }
+
+    public int getShow_id() {
+        return show_id;
+    }
+
+    public void setShow_id(int show_id) {
+        this.show_id = show_id;
+    }
+
+    public double getShowP_price() {
+        return showP_price;
+    }
+
+    public void setShowP_price(double showP_price) {
+        this.showP_price = showP_price;
+    }
 }
--- comm/src/main/java/com/comm/comm/entity/ShowText.java
+++ comm/src/main/java/com/comm/comm/entity/ShowText.java
@@ -9,11 +9,35 @@ public class ShowText {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//自动增长
     @Column(name = "showTe_id", columnDefinition = "integer(11) COMMENT '演出简介ID'")
-    private int id;
-
-    @Column(name = "show_id", columnDefinition = "integer(11) COMMENT '演出ID'")
     private int showTe_id;
 
+    @Column(name = "show_id", columnDefinition = "integer(11) COMMENT '演出ID'")
+    private int show_id;
+
     @Column(name = "showTe_text", columnDefinition = "text COMMENT '演出简介'")
-    private String con;
+    private String showTe_text;
+
+    public int getShowTe_id() {
+        return showTe_id;
+    }
+
+    public void setShowTe_id(int showTe_id) {
+        this.showTe_id = showTe_id;
+    }
+
+    public int getShow_id() {
+        return show_id;
+    }
+
+    public void setShow_id(int show_id) {
+        this.show_id = show_id;
+    }
+
+    public String getShowTe_text() {
+        return showTe_text;
+    }
+
+    public void setShowTe_text(String showTe_text) {
+        this.showTe_text = showTe_text;
+    }
 }

30a062e5739895640fb500c56e77d3abecd491cd
JreAx <15035661726@163.com>
2018-12-26 14:57:37 +0800

    Revert "刘鑫"
    This reverts commit 7df9103

--- comm/src/main/java/com/comm/comm/CommApplication.java
+++ comm/src/main/java/com/comm/comm/CommApplication.java
@@ -1,11 +1,9 @@
 package com.comm.comm;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class CommApplication {
 
     public static void main(String[] args) {
